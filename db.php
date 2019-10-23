75b770a7d1f7b9903df30553f6b93263c101514f
Anfr01001 <46518215+Anfr01001@users.noreply.github.com>
2019-10-23 10:45:15 +0200

    Enkel databas koppling

---
+++ db.php
@@ -0,0 +1,24 @@
+<?php
+$db = new mysqli("localhost","dbuser","abc123","webbshop");
+
+if (!$db){
+	echo "Connection failed";
+	exit;
+}
+
+$sql = "SELECT * FROM products";
+
+$result = $db->query($sql);
+
+while ($row=$result->fetch_assoc()){
+	echo $row['name'];
+	echo", ";
+	echo $row['description'];
+	echo", ";
+	echo $row['price'];
+	echo", ";
+	echo $row['picture'];
+	echo"<br> ";
+}
+
+?>
